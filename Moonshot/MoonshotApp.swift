053b4efdb4caefdb1ce6cb4198bd691e169ed981
Namgyal <sasnams.e.sxd162001@gmail.com>
2024-12-03 15:12:15 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Namgyal Thily on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

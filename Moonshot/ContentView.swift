1ad23e190d60e2f8692c7d028f94544adfad3735
Namgyal <sasnams.e.sxd162001@gmail.com>
2024-12-03 23:59:47 -0500

    [feature] - converting json string to swift struct using codable and JSONDecoder

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+struct User: Codable{
+    let name : String
+    let address: Address
+}
+
+struct Address: Codable{
+    let street: String
+    let city: String
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            List(0..<100, id: \.self){ row in
-                NavigationLink("Row \(row)"){
-                    Text("Detail \(row)")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
+            """
+            let data = Data(input.utf8)
+            if let user = try? JSONDecoder().decode(User.self, from: data){
+                print(user.address.city)
+                print(user)
+                print(type(of:user))
+            }
+            
+
         }
+        
+        
     
            
     }
